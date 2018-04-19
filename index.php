9bd87a569ccb3df05b41ba0f4742aad4a633e1df
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-04-18 21:30:08 -0700

    Update index.php

--- index.php
+++ index.php
@@ -50,8 +50,14 @@ function NASPlayer(){
     <h1><a href="https://github.com/cjtrowbridge/NAS-Player" target="_blank">NAS-Player</a></h1>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
     
-    <?php NASPlayer(); ?>
-
+    <div class="container">
+      <div class="row">
+        <div col-12">
+          <?php NASPlayer(); ?>
+        </div>
+      </div>
+    </div>
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
