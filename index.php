37eb0ae8d0163dffc7a3755a0e97340b227be2bd
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-04-18 21:25:47 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+$DirectorySVG = "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 22 22\"><defs><clipPath><path d=\"m69.63 12.145h-.052c-22.727-.292-46.47 4.077-46.709 4.122-2.424.451-4.946 2.974-5.397 5.397-.044.237-4.414 23.983-4.122 46.71-.292 22.777 4.078 46.523 4.122 46.761.451 2.423 2.974 4.945 5.398 5.398.237.044 23.982 4.413 46.709 4.121 22.779.292 46.524-4.077 46.761-4.121 2.423-.452 4.946-2.976 5.398-5.399.044-.236 4.413-23.981 4.121-46.709.292-22.777-4.077-46.523-4.121-46.761-.453-2.423-2.976-4.946-5.398-5.397-.238-.045-23.984-4.414-46.71-4.122\"/></clipPath><clipPath><rect y=\"507.8\" x=\"392.57\" height=\"32\" width=\"32\" fill=\"none\" rx=\"16\"/></clipPath><clipPath><rect y=\"8\" x=\"8\" height=\"32\" width=\"32\" fill=\"none\" rx=\"16\"/></clipPath><clipPath><rect y=\"507.8\" x=\"392.57\" height=\"32\" width=\"32\" fill=\"none\" rx=\"16\"/></clipPath><clipPath><rect y=\"507.8\" x=\"392.57\" height=\"32\" width=\"32\" fill=\"none\" rx=\"16\"/></clipPath><clipPath><rect width=\"32\" height=\"32\" x=\"8\" y=\"8\" fill=\"none\" rx=\"16\"/></clipPath><clipPath><rect width=\"32\" height=\"32\" x=\"392.57\" y=\"507.8\" fill=\"none\" rx=\"16\"/></clipPath><clipPath><rect width=\"32\" height=\"32\" x=\"392.57\" y=\"507.8\" fill=\"none\" rx=\"16\"/></clipPath><clipPath><rect width=\"32\" height=\"32\" x=\"392.57\" y=\"507.8\" fill=\"none\" rx=\"16\"/></clipPath><clipPath><rect y=\"507.8\" x=\"392.57\" height=\"32\" width=\"32\" fill=\"none\" rx=\"16\"/></clipPath><clipPath><rect y=\"507.8\" x=\"392.57\" height=\"32\" width=\"32\" fill=\"none\" rx=\"16\"/></clipPath><clipPath><rect y=\"8\" x=\"8\" height=\"32\" width=\"32\" fill=\"none\" rx=\"16\"/></clipPath><clipPath><path d=\"m0 706.47h1490.93v-706.47h-1490.93v706.47\"/></clipPath><clipPath><path d=\"m22.2 686.12h1447.73v-667.19h-1447.73v667.19\"/></clipPath><clipPath><rect y=\"507.8\" x=\"392.57\" height=\"32\" width=\"32\" fill=\"none\" rx=\"16\"/></clipPath><clipPath><rect y=\"507.8\" x=\"392.57\" height=\"32\" width=\"32\" fill=\"none\" rx=\"16\"/></clipPath><clipPath><rect y=\"507.8\" x=\"392.57\" height=\"32\" width=\"32\" fill=\"none\" rx=\"16\"/></clipPath><clipPath><rect y=\"507.8\" x=\"392.57\" height=\"32\" width=\"32\" fill=\"none\" rx=\"16\"/></clipPath><clipPath><rect y=\"8\" x=\"8\" height=\"32\" width=\"32\" fill=\"none\" rx=\"16\"/></clipPath></defs><path d=\"m43.887-36.699c-1.108 0-2 .892-2 2v81.61l111 .24v-70.04c0-1.108-.892-2-2-2h-60.17c-2.039.18-2.301-.09-3.308-1.435-1.429-1.912-3.586-7.06-4.992-8.929-1.129-1.633-1.312-1.443-3.184-1.443z\" color=\"#000\" transform=\"matrix(.19082 0 0 .19082-7.58 10.02)\" fill=\"#4d4d4d\"/></svg>";
+$FileSVG      = "<?xml version=\"1.0\" encoding=\"utf-8\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.0//EN\" \"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\" [<!ENTITY ns_svg \"http://www.w3.org/2000/svg\"><!ENTITY ns_xlink \"http://www.w3.org/1999/xlink\">]><svg version=\"1.0\"id=\"svg4198\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" inkscape:version=\"0.43+devel\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns:cc=\"http://web.resource.org/cc/\" sodipodi:docbase=\"/home/tigert/cvs/freedesktop.org/tango-icon-theme/scalable/actions\" sodipodi:docname=\"edit-copy.svg\" inkscape:output_extension=\"org.inkscape.output.svg.inkscape\" xmlns:inkscape=\"http://www.inkscape.org/namespaces/inkscape\" xmlns:sodipodi=\"http://inkscape.sourceforge.net/DTD/sodipodi-0.dtd\" sodipodi:version=\"0.32\"xmlns=\"&ns_svg;\" xmlns:xlink=\"&ns_xlink;\" width=\"32.011\" height=\"37.052\"viewBox=\"0 0 32.011 37.052\"overflow=\"visible\" enable-background=\"new 0 0 595.275 841.89\" xml:space=\"preserve\"><sodipodi:namedview showgrid=\"false\" borderopacity=\"1.0000000\" pagecolor=\"#ffffff\" inkscape:cy=\"48.133585\" id=\"base\" inkscape:zoom=\"1.4142136\" inkscape:pageopacity=\"0.0\" inkscape:current-layer=\"layer1\" inkscape:grid-bbox=\"true\" inkscape:window-width=\"873\" inkscape:window-y=\"242\" inkscape:window-height=\"699\" inkscape:window-x=\"264\" inkscape:pageshadow=\"2\" inkscape:document-units=\"px\" inkscape:showpageshadow=\"false\" inkscape:cx=\"56.648344\" bordercolor=\"#bebebe\"></sodipodi:namedview><g><linearGradient id=\"rect12413_1_\" gradientUnits=\"userSpaceOnUse\" x1=\"-9.9282\" y1=\"-786.1836\" x2=\"3.5489\" y2=\"-765.6773\" gradientTransform=\"matrix(1.0672 0 0 -0.9893 23.2239 -754.4793)\"><stop offset=\"0\" style=\"stop-color:#F0F0EF\"/><stop offset=\"0.5993\" style=\"stop-color:#E8E8E8\"/><stop offset=\"0.8276\" style=\"stop-color:#FFFFFF\"/><stop offset=\"1\" style=\"stop-color:#D8D8D3\"/></linearGradient><path id=\"rect12413\" sodipodi:nodetypes=\"ccccccccc\" fill=\"url(#rect12413_1_)\" stroke=\"#888A85\" d=\"M0.5,35.984V1.081c0-0.314,0.254-0.567,0.57-0.567h21.159c2.389,0,9.268,5.827,9.268,8.303v27.167c0,0.314-0.254,0.566-0.568,0.566H1.07C0.755,36.553,0.5,36.299,0.5,35.984z\"/><linearGradient id=\"path2210_1_\" gradientUnits=\"userSpaceOnUse\" x1=\"22.3452\" y1=\"-761.5874\" x2=\"20.0135\" y2=\"-764.2749\" gradientTransform=\"matrix(1 0 0 -1 6.1611 -758.7207)\"><stop offset=\"0\" style=\"stop-color:#7C7C7C\"/><stop offset=\"1\" style=\"stop-color:#B8B8B8\"/></linearGradient><path id=\"path2210\" sodipodi:nodetypes=\"cccc\" fill=\"url(#path2210_1_)\" stroke=\"#868A84\" d=\"M22.635,7.568c4.107,0.159,7.305-0.978,8.867,1.446c0.305-3.968-7.254-8.828-9.286-8.498C23.03,1.015,22.635,7.568,22.635,7.568z\"/><linearGradient id=\"path2247_1_\" gradientUnits=\"userSpaceOnUse\" x1=\"-246.8369\" y1=\"-357.894\" x2=\"-246.0629\" y2=\"-356.745\"><stop offset=\"0\" style=\"stop-color:#FFFFFF\"/><stop offset=\"1\" style=\"stop-color:#FFFFFF\"/></linearGradient><path id=\"path2247\" sodipodi:nodetypes=\"cccc\" opacity=\"0.3693\" fill=\"none\" stroke=\"url(#path2247_1_)\" enable-background=\"new \" d=\"M23.694,6.544c2.754-0.019,4.107-0.49,5.703,0.19c-1.299-1.881-4.359-3.344-5.729-4.028C23.856,3.482,23.694,6.544,23.694,6.544z\"/></g></svg>";
+
+//TODO make this automatically know wherever it is
+$Path = '/var/www';
+
+if(isset($_GET['path']))){
+  //Validate Path
+  
+}else{
+  $Path .= '/';
+}
+
+function NASPlayer(){
+  $Directories=array();
+  $Files=array();
+  if($Handle = opendir($Path)){
+    while(false !== ($Entry = readdir($Fandle))){
+      if(is_dir($Path.DIRECTORY_SEPARATOR.$Entry)){
+        if(($Entry !== '.')&& ($Entry!=='..')){
+          $Directories[$Entry]=$Path.DIRECTORY_SEPARATOR.$Entry;
+        }
+      }else{
+        $Files[$Entry]=$Path.DIRECTORY_SEPARATOR.$Entry;
+      }
+    }
+    closedir($Handle);
+  }
+  asort($Directories);
+  asort($Files);
+  foreach($Directories as $Name => $Directory){
+    echo '<div><a href="'.$Name.'">'.$DirectorySVG.' '.$Name.'</a><br></div>';
+  }
+  foreach($files as $name => $file){
+    echo '<div><a href="'.$Name.'">'.$FileSVG.' '.$Name.'</a><br></div>';
+  }
+}
+
+?><!DOCTPYE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>NAS-Player</title>
+  </head>
+  <body>
+    <h1><a href="https://github.com/cjtrowbridge/NAS-Player" target="_blank">NAS-Player</a></h1>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
+    
+    <?php NASPlayer(); ?>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
